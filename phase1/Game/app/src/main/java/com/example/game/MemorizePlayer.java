9917f552323d4f63ca6e54df9c161ac785685e2f
Rutvik <rutvikragupta@gmail.com>
2019-10-26 16:46:35 -0400

    Added a background to the layout, might change it later.

--- phase1/Game/app/src/main/java/com/example/game/MemorizePlayer.java
+++ phase1/Game/app/src/main/java/com/example/game/MemorizePlayer.java
@@ -1,13 +1,22 @@
 package com.example.game;
 
 
+import android.widget.TextView;
+
 /**A Player of Memorize Game */
 public class MemorizePlayer {
     /** Total points earned by the Player*/
     private int pointsEarned;
+    /** The Text on the screen which keeps track if the points earned by the player*/
+    private TextView textView;
 
-    public MemorizePlayer() {
+    public MemorizePlayer(TextView tv) {
         pointsEarned = 0;
+        this.textView = tv;
+    }
+
+    public void setTextView(TextView new_tv) {
+        this.textView = new_tv;
     }
 
     public int getPointsEarned() {
