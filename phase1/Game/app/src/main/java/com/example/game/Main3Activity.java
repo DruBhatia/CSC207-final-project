9d4974e3c36d3269704d1614340306f7048026ad
sangwan2 <kartik.sangwan@mail.utoronto.ca>
2019-10-26 22:54:27 -0400

    Removed the title and made the app to occupy the fullscreen for the game. Method OnCreate().

--- phase1/Game/app/src/main/java/com/example/game/Main3Activity.java
+++ phase1/Game/app/src/main/java/com/example/game/Main3Activity.java
@@ -3,6 +3,8 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        // Set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Set No Title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 }
