9d2178b443caea9aa0ecd93edc299501bd0bb37c
Rutvik <rutvikragupta@gmail.com>
2019-10-27 15:44:12 -0400

    Added a method in MemoryView.java to set an ojava.nclickListener on all the cards and removed another from PlayingCard.

--- phase1/Game/app/src/main/java/com/example/game/PlayingCard.java
+++ phase1/Game/app/src/main/java/com/example/game/PlayingCard.java
@@ -17,23 +17,19 @@ public class PlayingCard {
   PlayingCard(int card_num, ImageView iv, int ai) {
     this.cardNum = card_num;
     this.imageview = iv;
+    this.imageview.setTag(card_num);
     this.actualImage = ai;
   }
-  /** Set an on click listener on the image view of the card */
-  // https://developer.android.com/reference/android/view/View.OnClickListener used to learn.
-  void setOnClick() {
-    this.imageview.setOnClickListener(
-        new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-            // Set the image of card to the image view
-            imageview.setImageResource(cardNum);
-          }
-        });
+
+  /** Get the image view of the card */
+  ImageView getImageview(){
+    return this.imageview;
   }
+
+
   /** Set the front view of the card to the image */
   void setImage() {
-    this.imageview.setImageResource(cardNum);
+    this.imageview.setImageResource(this.actualImage);
   }
 
   /** Set whether the card will be visible on screen or not */
