912dc2c07adaf319620dd4346208c4afb57257c5
sangwan2 <kartik.sangwan@mail.utoronto.ca>
2019-10-27 14:47:15 -0400

    added the surface created, destroyed changed methods in the class and implemented SurfaceHolder.callback

--- phase1/Game/app/src/main/java/com/example/game/DonutView.java
+++ phase1/Game/app/src/main/java/com/example/game/DonutView.java
@@ -1,9 +1,11 @@
 package com.example.game;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -11,42 +13,51 @@ import android.view.View;
 
 public class DonutView extends SurfaceView implements SurfaceHolder.Callback {
 
-    private Bitmap Donut;
-
-    private Bitmap Ant;
-
+    private GameThread gameThread;
+    private Ant ant;
 
     public DonutView(Context context) {
         super(context);
         this.setFocusable(true);
         this.getHolder().addCallback(this);
-        Donut = BitmapFactory.decodeResource(getResources(), R.drawable.donut);
-
-        Ant = BitmapFactory.decodeResource(getResources(), R.drawable.ant);
+        Bitmap antImg = BitmapFactory.decodeResource(getResources(), R.drawable.ant);
+        gameThread = new GameThread(this.getHolder(),this);
+        ant = new Ant(antImg,800,800,this);
     }
 
-    public void update(Canvas canvas) {
-        //need to add this method in the Ant class.
+    public void update() {
+        ant.update();
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
+        ant.draw(canvas);
 
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        gameThread = new GameThread(this.getHolder(),this);
+        gameThread.setRunning(true);
+        gameThread.start();
 
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        boolean retry = true;
+        while(retry){
+            try{
+                gameThread.setRunning(false);
+                gameThread.join();
+            } catch (Exception e) { e.printStackTrace(); }
+            retry = false;
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
 
     }
 
