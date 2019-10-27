12c94bb6b2e2b952b82af7494d0db0798383a247
sangwan2 <kartik.sangwan@mail.utoronto.ca>
2019-10-27 14:49:10 -0400

    added setters for x and y instance variables.

--- phase1/Game/app/src/main/java/com/example/game/GameCreature.java
+++ phase1/Game/app/src/main/java/com/example/game/GameCreature.java
@@ -27,6 +27,14 @@ public class GameCreature {
         return height;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public int getX() {
         return x;
     }
