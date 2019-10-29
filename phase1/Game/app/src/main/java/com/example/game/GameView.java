39adc96cb5be9ea092da47f45db67e06730214d8
Tanmay Gupta <me.tanmaygupta@gmail.com>
2019-10-29 02:53:33 -0400

    "Setting up canvas to prepare it for elements for the game. Took help from: https://www.udemy.com/course/android-game-development-course/"

--- phase1/Game/app/src/main/java/com/example/game/GameView.java
+++ phase1/Game/app/src/main/java/com/example/game/GameView.java
@@ -1,20 +1,52 @@
 package com.example.game;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.view.Display;
 import android.view.View;
+import android.os.Handler;
 
 class GameView extends View {
   //This will be the custom view class for Game3
 
+  Handler handler;
+  Runnable runnable;
+  final int delayNum = 30;
+  Bitmap background;
+  Display display;
+  Point point;
+  int screenWidth, screenHeight; //Height and Width of device.
+  Rect rect;
+
+
   public GameView(Context context) {
     super(context);
+    handler = new Handler();
+    runnable = new Runnable() {
+      @Override
+      public void run() {
+        invalidate(); //This should call onDraw.
+      }
+    };
+    background = BitmapFactory.decodeResource(getResources(), R.drawable.game3_background);
+    display = ((Activity)getContext()).getWindowManager().getDefaultDisplay();
+    point = new Point();
+    display.getSize(point);
+    screenWidth = point.x;
+    screenHeight = point.y;
+    rect = new Rect(0, 0, screenWidth, screenHeight);
   }
 
   @Override
   protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
     //This is where we will draw our view for Game3.
-
+    canvas.drawBitmap(background, null, rect, null);
+    handler.postDelayed(runnable, delayNum);
   }
 }
