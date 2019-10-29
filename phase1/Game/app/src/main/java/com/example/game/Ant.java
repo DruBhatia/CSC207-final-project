f4079e6898fdbe57d971e87a78651b33163e1a46
sangwan2 <kartik.sangwan@mail.utoronto.ca>
2019-10-29 09:22:23 -0400

    Added a numAntsReached variable that keeps track of the number of ants that have reached the donut and thus call a method in DonutView to deccrease the life of the player and update the livesleft.

--- phase1/Game/app/src/main/java/com/example/game/Ant.java
+++ phase1/Game/app/src/main/java/com/example/game/Ant.java
@@ -7,6 +7,7 @@ class Ant extends GameCreature{
 
     private int speed;
     private DonutView donutView;
+    static int numAntsOffReached = 0;
 
     Ant(Bitmap image, int x, int y, DonutView donutView, int speed) {
         super(image, x, y);
@@ -22,9 +23,11 @@ class Ant extends GameCreature{
         //for now it just moves the ant in one direction will change to random later.
         int randomSpeed = (int) (Math.random() * speed * 2);
         this.setY(this.getY() - randomSpeed);
-        if (this.getY() < -100) {
+        if (this.getY() < 500) {
             this.setY(3000);
             this.speed += 1;
+            numAntsOffReached++;
+            donutView.decreaseLife();
         }
     }
 }
