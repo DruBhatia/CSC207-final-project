abee700e760607cec3d3b95c8ecca989449157b5
sangwan2 <kartik.sangwan@mail.utoronto.ca>
2019-10-30 11:08:20 -0400

    Added the AntBegin class activity that is launched when intent2 from the main activity is called.It is basically the home screen for AntCrusher game.

---
+++ phase1/Game/app/src/main/java/com/example/game/AntBegin.java
@@ -0,0 +1,31 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class AntBegin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ant_begin);
+        ImageButton ant = findViewById(R.id.imageButton3);
+
+        ant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent2 = new Intent(AntBegin.this, Main3Activity.class);
+                startActivity(intent2);
+            }
+        });
+    }
+
+    protected void highScore(){
+        //Create a high score pop up screen on later here.
+    }
+
+}
