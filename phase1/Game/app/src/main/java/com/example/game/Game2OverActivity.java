2c8351583dc7705070b3e2719d3a5fad6e9a265f
Rutvik <rutvikragupta@gmail.com>
2019-10-30 01:13:35 -0400

    Extended the Game2OverActivity to set onClick activities on the two buttons on layout and set the number of moves left and status accordingly.

--- phase1/Game/app/src/main/java/com/example/game/Game2OverActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/Game2OverActivity.java
@@ -2,7 +2,13 @@ package com.example.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Objects;
 
@@ -15,6 +21,36 @@ public class Game2OverActivity extends AppCompatActivity {
     int showMoves = (int) Objects.requireNonNull(getIntent().getExtras()).get("Moves Left");
     boolean cardsLeft =
         (boolean) Objects.requireNonNull(getIntent().getExtras()).get("Cards Left to Match");
+
+    Button mainMenu = findViewById(R.id.main);
+    Button restart = findViewById(R.id.playAgain);
+    TextView gameStatus = findViewById(R.id.status);
+    TextView movesLeft = findViewById(R.id.moves_left);
+
+      restart.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent intent1 = new Intent(Game2OverActivity.this, MemoryBegin.class);
+              startActivity(intent1);
+          }
+      });
+
+      mainMenu.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent intent3 = new Intent(Game2OverActivity.this, MainActivity.class);
+              startActivity(intent3);
+          }
+      });
+      if (cardsLeft && showMoves >= 0) {
+          gameStatus.setText("YOU WON");
+      }
+      else if (!cardsLeft && showMoves == 0) {
+          gameStatus.setText("YOU LOST :(");
+      }
+
+          movesLeft.setText("Moves Left: " + showMoves);
+
 
 
 
