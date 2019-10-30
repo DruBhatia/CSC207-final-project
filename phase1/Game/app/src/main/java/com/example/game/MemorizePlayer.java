852b39bd24cb7626ebab48a0d1ee0c86a3501fa2
Rutvik <rutvikragupta@gmail.com>
2019-10-30 19:33:22 -0400

    Added a chronometer in memory_game.xml which keeps track of the time elapsed to complete the game and created and instance variable of it in the MemorizedPlayer finally initalized in memoryView.

--- phase1/Game/app/src/main/java/com/example/game/MemorizePlayer.java
+++ phase1/Game/app/src/main/java/com/example/game/MemorizePlayer.java
@@ -1,30 +1,37 @@
 package com.example.game;
 
+import android.widget.Chronometer;
 import android.widget.TextView;
 
-/**A Player of Memorize Game */
+/** A Player of Memorize Game */
 public class MemorizePlayer {
-    /** Total moves available to the Player*/
-    private int playerMoves;
-    /** The Text on the screen which keeps track if the points earned by the player*/
-    private TextView textView;
+  /** Total moves available to the Player */
+  private int playerMoves;
+  /** The Text on the screen which keeps track of the points earned by the player */
+  private TextView textView;
+  /** The stop watch which keeps track of the time spent by the player to complete the game */
+  private Chronometer chronometer;
 
-    public MemorizePlayer(TextView tv) {
-        playerMoves = 15;
-        this.textView = tv;
-    }
+  public MemorizePlayer(TextView tv, Chronometer meter) {
+    playerMoves = 15;
+    this.textView = tv;
+    this.chronometer = meter;
+  }
 
-    void setTextMoves() {
-        String new_text =  "Moves Left:" + this.getMovesLeft();
-        this.textView.setText(new_text);
-    }
+  void setTextMoves() {
+    String new_text = "Moves Left:" + this.getMovesLeft();
+    this.textView.setText(new_text);
+  }
 
-    int getMovesLeft() {
-        return playerMoves;
-    }
+  Chronometer getChronometer() {
+    return chronometer;
+  }
 
-    void decreasePlayerMoves() {
-        this.playerMoves--;
-    }
+  int getMovesLeft() {
+    return playerMoves;
+  }
 
+  void decreasePlayerMoves() {
+    this.playerMoves--;
+  }
 }
