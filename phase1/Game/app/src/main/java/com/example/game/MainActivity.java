384a5c4e04869e3113fefc0a2b68845b1a422fb3
bhatiad6 <dhruov.bhatia@utoronto.edu>
2019-10-31 01:00:30 -0400

    Added functionality to the 'Information button', made a new layout and activity to explain all game rules.

--- phase1/Game/app/src/main/java/com/example/game/MainActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/MainActivity.java
@@ -23,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
         Intent intent1 = new Intent(this, MemoryBegin.class);
         Intent intent2 = new Intent(this, AntBegin.class);
         Intent intent3 = new Intent(this, Main4Activity.class);
+        Intent intent4 = new Intent(this, InformationActivity.class);
         if(R.id.button1 == view.getId()) startActivity(intent1);
         else if (R.id.button5 == view.getId()) startActivity(intent2);
         else if (R.id.button6 == view.getId()) startActivity(intent3);
+        else if (R.id.button7 == view.getId()) startActivity(intent4);
     }
 
 }
