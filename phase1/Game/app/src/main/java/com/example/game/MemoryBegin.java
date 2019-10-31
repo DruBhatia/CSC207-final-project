289abab09a53b93ed53be05e2160a05eecfd33ed
bhatiad6 <dhruov.bhatia@utoronto.edu>
2019-10-31 05:24:31 -0400

    Attempt to synchronize files

--- phase1/Game/app/src/main/java/com/example/game/MemoryBegin.java
+++ phase1/Game/app/src/main/java/com/example/game/MemoryBegin.java
@@ -18,17 +18,9 @@ public class MemoryBegin extends AppCompatActivity {
 
         playMemory.setOnClickListener(new View.OnClickListener() {
             @Override
-                    public void onClick(View v){
+            public void onClick(View v){
                 Intent intent2 = new Intent(MemoryBegin.this, Main2Activity.class);
                 startActivity(intent2);
-            }
-        });
-
-        playMemoryDark.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent intent3 = new Intent(MemoryBegin.this, Main2ActivityDark.class);
-                startActivity(intent3);
             }
         });
 
