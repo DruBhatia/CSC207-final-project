92bd103d28a4890e4b29d6c9e7ee7fbdaa9104fe
bhatiad6 <dhruov.bhatia@utoronto.edu>
2019-10-31 03:39:36 -0400

    Added new class and activity for playing in 'dark mode' theme.

---
+++ phase1/Game/app/src/main/java/com/example/game/MemoryView2.java
@@ -0,0 +1,190 @@
+package com.example.game;
+
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MemoryView2 extends MemoryView {
+
+    private int cardBack = R.drawable.bv_01;
+
+  MemoryView2(AppCompatActivity context) {
+    super(context);
+    this.initializeCards(context);
+      player =
+              new MemorizePlayer(
+                      (TextView) context.findViewById(R.id.text_moves),
+                      (TextView) context.findViewById(R.id.text_points),
+                      (Chronometer) context.findViewById(R.id.stopWatch));
+      this.initializeCards(context);
+      show();
+      player.getChronometer().setBase(SystemClock.elapsedRealtime());
+      player.getChronometer().start();
+      setOnClick();
+    }
+
+
+    @Override
+    void initializeCards(AppCompatActivity context){
+        cardArray = new ArrayList<>();
+        imageArray = new ArrayList<>();
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_11)));
+        int[] arr1 = new int[] {101, R.drawable.fv_image1d1};
+        imageArray.add(arr1);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_12)));
+        int[] arr2 = new int[] {102, R.drawable.fv_image1d2};
+        imageArray.add(arr2);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_13)));
+        int[] arr3 = new int[] {103, R.drawable.fv_image1d3};
+        imageArray.add(arr3);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_14)));
+        int[] arr4 = new int[] {104, R.drawable.fv_image1d4};
+        imageArray.add(arr4);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_21)));
+        int[] arr5 = new int[] {105, R.drawable.fv_image1d5};
+        imageArray.add(arr5);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_22)));
+        int[] arr6 = new int[] {106, R.drawable.fv_image1d6};
+        imageArray.add(arr6);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_23)));
+        int[] arr7 = new int[] {107, R.drawable.fv_image1d7};
+        imageArray.add(arr7);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_24)));
+        int[] arr8 = new int[] {108, R.drawable.fv_image1d8};
+        imageArray.add(arr8);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_31)));
+        int[] arr9 = new int[] {201, R.drawable.fv_image2d1};
+        imageArray.add(arr9);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_32)));
+        int[] arr10 = new int[] {202, R.drawable.fv_image2d2};
+        imageArray.add(arr10);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_33)));
+        int[] arr11 = new int[] {203, R.drawable.fv_image2d3};
+        imageArray.add(arr11);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_34)));
+        int[] arr12 = new int[] {204, R.drawable.fv_image2d4};
+        imageArray.add(arr12);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_41)));
+        int[] arr13 = new int[] {205, R.drawable.fv_image2d5};
+        imageArray.add(arr13);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_42)));
+        int[] arr14 = new int[] {206, R.drawable.fv_image2d6};
+        imageArray.add(arr14);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_43)));
+        int[] arr15 = new int[] {207, R.drawable.fv_image2d7};
+        imageArray.add(arr15);
+        cardArray.add(new PlayingCard((ImageView) context.findViewById(R.id.iv_44)));
+        int[] arr16 = new int[] {208, R.drawable.fv_image2d8};
+        imageArray.add(arr16);
+        Collections.shuffle(imageArray);
+    }
+
+    @Override
+    void show() {
+        for (PlayingCard playCard : cardArray) {
+            playCard.set_enable(false);
+        }
+        for (PlayingCard playCard : cardArray) {
+            int image = imageArray.get(cardArray.indexOf(playCard))[1];
+            playCard.setImage(image);
+        }
+        Handler handler = new Handler();
+        handler.postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        for (PlayingCard playCard : cardArray) {
+                            playCard.setImage(R.drawable.bv_01);
+                        }
+                        for (PlayingCard playCard : cardArray) {
+                            playCard.set_enable(true);
+                        }
+                    }
+                },2500);
+    }
+
+    private void setSelection(PlayingCard c) {
+        // Set the image of card to the image view
+        int image = imageArray.get(cardArray.indexOf(c))[1];
+        c.setImage(image);
+        // check the selection of two cards and store them temporarily
+        if (cardNum == 1) {
+            firstCard = imageArray.get(cardArray.indexOf(c))[0];
+            if (firstCard > 200) {
+                firstCard = firstCard - 100;
+            }
+            cardNum = 2;
+            firstSelect = this.cardArray.indexOf(c);
+            // Make this card unresponsive
+            c.set_enable(false);
+
+        } else {
+            secondCard = imageArray.get(cardArray.indexOf(c))[0];
+            if (secondCard > 200) {
+                secondCard = secondCard - 100;
+            }
+            cardNum = 1;
+            secondSelect = this.cardArray.indexOf(c);
+            // Make all cards unresponsive
+            for (PlayingCard playCard : cardArray) {
+                playCard.set_enable(false);
+            }
+            // https://developer.android.com/reference/android/os/Handler.html &
+            // https://stackoverflow.com/questions/15136199/when-to-use-handler-post-when-to-new-thread
+            // After selecting the two cards delay the game by 100 milliseconds to check whether the two
+            // cards match or not and proceed ahead.
+            Handler handler = new Handler();
+            handler.postDelayed(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            compare();
+                        }
+                    },
+                    400);
+        }
+    }
+
+    private void compare() {
+        // If card matches make them invisible
+        if (firstCard == secondCard) {
+            cardArray.get(firstSelect).setVisibility();
+            cardArray.get(secondSelect).setVisibility();
+            // Increase the points by 2 for correct match
+            player.increasePlayerPoints();
+            player.setTextPoints();
+        } else {
+            // Decrease the points by 1 for incorrect match
+            player.decreasePlayerPoints();
+            player.setTextPoints();
+            // Load back the front images again if
+            for (PlayingCard card : cardArray) {
+                card.setImage(cardBack);
+            }
+        }
+        player.decreasePlayerMoves();
+        player.setTextMoves();
+        // Make all cards responsive again
+        for (PlayingCard card : cardArray) {
+            card.set_enable(true);
+        }
+        endGame(checkEnd());
+    }
+
+    boolean checkVisibility() {
+        for (PlayingCard item : cardArray) {
+            if (item.getVisibility() != View.INVISIBLE) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
