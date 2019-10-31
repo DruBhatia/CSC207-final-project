61e5315858ae15ca61484a3d87bafb0be5dfde16
Tanmay Gupta <me.tanmaygupta@gmail.com>
2019-10-30 23:46:10 -0400

    "Created a separate class for CN Tower called Tower.java. GameView class uses the Tower class by composition."

--- phase1/Game/app/src/main/java/com/example/game/GameView.java
+++ phase1/Game/app/src/main/java/com/example/game/GameView.java
@@ -15,24 +15,16 @@ import java.util.Random;
 
 public class GameView extends View {
   // This will be the custom view class for Game3
-  TrueBlue tb; // trueblue class
+  TrueBlue tb; // TrueBlue class
+  Tower cn; // Tower class
   Handler handler;
   Runnable runnable;
   final int delayNum = 30;
   Bitmap background;
-  Bitmap topTower, bottomTower;
   Display display;
   Point point;
   int screenWidth, screenHeight; // Height and Width of device.
   Rect rect;
-  int distance = 500; // distance between top and bottom CN Tower.
-  int minTower, maxTower;
-  int numOfTowers = 4;
-  int distBetweenTowers;
-  int[] cnX = new int[numOfTowers];
-  int[] cnY = new int[numOfTowers];
-  Random random;
-  int towerVelocity = 7;
 
   public GameView(Context context) {
     super(context);
@@ -45,24 +37,16 @@ public class GameView extends View {
           }
         };
     background = BitmapFactory.decodeResource(getResources(), R.drawable.game3_background);
-    topTower = BitmapFactory.decodeResource(getResources(), R.drawable.cn_tower_down);
-    bottomTower = BitmapFactory.decodeResource(getResources(), R.drawable.cn_tower_up);
     display = ((Activity) getContext()).getWindowManager().getDefaultDisplay();
     point = new Point();
     display.getSize(point);
     screenWidth = point.x;
     screenHeight = point.y;
     rect = new Rect(0, 0, screenWidth, screenHeight);
-    distBetweenTowers = screenWidth * 3 / 4;
-    minTower = distance / 2;
-    maxTower = screenHeight - minTower - distance;
-    random = new Random();
+    cn = new Tower(this);
+    cn.moveTower();
     // creates TrueBlue
     tb = new TrueBlue(this);
-    for (int i = 0; i < numOfTowers; i++) {
-      cnX[i] = screenWidth + i * distBetweenTowers;
-      cnY[i] = minTower + random.nextInt(maxTower - minTower + 1);
-    }
   }
 
   @Override
@@ -77,15 +61,7 @@ public class GameView extends View {
     // true blue falls
     if (tb.getState()) {
       tb.tbFall();
-      for (int i = 0; i < numOfTowers; i++) {
-        cnX[i] -= towerVelocity;
-        if (cnX[i] < -topTower.getWidth()) {
-          cnX[i] += numOfTowers * distBetweenTowers;
-          cnY[i] = minTower + random.nextInt(maxTower - minTower + 1);
-        }
-        canvas.drawBitmap(topTower, cnX[i], cnY[i] - topTower.getHeight(), null);
-        canvas.drawBitmap(bottomTower, cnX[i], cnY[i] + distance, null);
-      }
+      cn.drawTower(canvas);
     }
     // displays true blue in the center
     tb.drawTB(canvas);
