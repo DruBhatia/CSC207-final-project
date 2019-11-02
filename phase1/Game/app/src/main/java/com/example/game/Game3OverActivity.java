38322ff15d06c87ea78d94a9db33fda570985696
Rutvik <rutvikragupta@gmail.com>
2019-11-01 22:05:01 -0400

    Modified the layout of game3OverActivity to display game over and added buttons, removed some unnecessary imports and made loginActivity to display the username when logged in.

--- phase1/Game/app/src/main/java/com/example/game/Game3OverActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/Game3OverActivity.java
@@ -2,13 +2,35 @@ package com.example.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Game3OverActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game3_over);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_game3_over);
+    Button mainMenu = findViewById(R.id.menu3);
+    Button rePlayGame = findViewById(R.id.play3_again);
+    rePlayGame.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Intent intent2 = new Intent(Game3OverActivity.this, Main3Activity.class);
+            startActivity(intent2);
+          }
+        });
+
+    mainMenu.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Intent intent3 = new Intent(Game3OverActivity.this, Main1Activity.class);
+            startActivity(intent3);
+          }
+        });
+  }
 }
