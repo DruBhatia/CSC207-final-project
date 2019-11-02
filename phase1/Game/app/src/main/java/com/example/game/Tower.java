dee111b23817228ed2d6ca27f342854c4788de3c
Tanmay Gupta <me.tanmaygupta@gmail.com>
2019-11-01 22:22:16 -0400

    "Created a rectangle for score. When true blue passes through that rectangle, the score increases."

--- phase1/Game/app/src/main/java/com/example/game/Tower.java
+++ phase1/Game/app/src/main/java/com/example/game/Tower.java
@@ -23,6 +23,7 @@ class Tower {
   private GameView gv;
   private Rect topTowerRect;
   private Rect bottomTowerRect;
+  private Rect score;
 
 
   Tower(GameView gv) {
@@ -60,15 +61,18 @@ class Tower {
         cnY[i] = minTower + random.nextInt(maxTower - minTower + 1);
       }
       Paint p = new Paint();
-      p.setColor(Color.BLUE);
+      p.setARGB(128,255,255,255);
+      Paint s = new Paint();
       topTowerRect = new Rect(cnX[i], cnY[i] - topTower.getHeight(),
               cnX[i] + topTower.getWidth(),cnY[i] - topTower.getHeight() + topTower.getHeight());
       bottomTowerRect = new Rect(cnX[i], cnY[i] + distance,
               cnX[i] + bottomTower.getWidth(), cnY[i] + distance + bottomTower.getHeight());
+      score = new Rect(cnX[i], cnY[i], cnX[i] + topTower.getWidth(), cnY[i] + distance);
       canvas.drawBitmap(topTower, null, topTowerRect, null);
       canvas.drawBitmap(bottomTower, null, bottomTowerRect, null  );
       canvas.drawRect(topTowerRect, p);
       canvas.drawRect(bottomTowerRect,p);
+      canvas.drawRect(score,s);
       Rect tb = gv.tb.getTbRect();
       if (getIntersectTb(bottomTowerRect, tb) || getIntersectTb(topTowerRect, tb)){
         gv.gameOver();
