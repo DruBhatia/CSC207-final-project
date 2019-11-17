54c99a6330ea70750d039f6936ffcaeb62b7fe8a
Rutvik <rutvikragupta@gmail.com>
2019-11-16 19:09:54 -0500

    Modified the class Game2OverActivity so that now the layout gets updated with statistics from the three levels.

--- phase2/Final_Game/app/src/main/java/com/example/final_game/Game2OverActivity.java
+++ phase2/Final_Game/app/src/main/java/com/example/final_game/Game2OverActivity.java
@@ -1,12 +1,12 @@
 package com.example.final_game;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Game2OverActivity extends AppCompatActivity {
 
@@ -14,18 +14,31 @@ public class Game2OverActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_game2_over);
-    String showMoves = getIntent().getExtras().get("Moves Left").toString();
-    String cardsLeft = getIntent().getExtras().get("Cards Left To Match?").toString();
-    String game_time = getIntent().getExtras().get("time").toString();
-    String score = getIntent().getExtras().get("points").toString();
+    String showMoves2 = getIntent().getExtras().get("Moves Left2").toString();
+    String showMoves3 = getIntent().getExtras().get("Moves Left3").toString();
+    String cardsLeft1 = getIntent().getExtras().get("Cards Left1").toString();
+    String cardsLeft2 = getIntent().getExtras().get("Cards Left2").toString();
+    String cardsLeft3 = getIntent().getExtras().get("Cards Left3").toString();
+    String game_time1 = getIntent().getExtras().get("time1").toString();
+    String score1 = getIntent().getExtras().get("points1").toString();
+    String game_time2 = getIntent().getExtras().get("time2").toString();
+    String score2 = getIntent().getExtras().get("points2").toString();
+    String game_time3 = getIntent().getExtras().get("time3").toString();
+    String score3 = getIntent().getExtras().get("points3").toString();
 
     Button mainMenu = findViewById(R.id.main);
     Button restart = findViewById(R.id.playAgain);
-    TextView gameStatus = findViewById(R.id.status);
-    TextView movesLeft = findViewById(R.id.moves_left);
-    TextView gameTime = findViewById(R.id.time);
-    TextView finalScore = findViewById(R.id.total_score);
-
+    TextView gameStatus1 = findViewById(R.id.status1);
+    TextView gameTime1 = findViewById(R.id.time1);
+    TextView finalScore1 = findViewById(R.id.score1);
+    TextView movesLeft2 = findViewById(R.id.moves_left2);
+    TextView gameStatus2 = findViewById(R.id.status2);
+    TextView gameTime2 = findViewById(R.id.time2);
+    TextView finalScore2 = findViewById(R.id.score2);
+    TextView movesLeft3 = findViewById(R.id.moves_left3);
+    TextView gameStatus3 = findViewById(R.id.status3);
+    TextView gameTime3 = findViewById(R.id.time3);
+    TextView finalScore3 = findViewById(R.id.score3);
     restart.setOnClickListener(
         new View.OnClickListener() {
           @Override
@@ -43,14 +56,28 @@ public class Game2OverActivity extends AppCompatActivity {
             startActivity(intent3);
           }
         });
-    if ((cardsLeft.equals("NO"))) {
-      gameStatus.setText("YOU WON!!");
-    } else if (cardsLeft.equals("YES") && showMoves.equals("0")) {
-      gameStatus.setText("YOU LOST :(");
+    if ((cardsLeft2.equals("NO"))) {
+      gameStatus2.setText("YOU WON!!");
+    } else if (cardsLeft2.equals("YES") && showMoves2.equals("0")) {
+      gameStatus2.setText("YOU LOST :(");
     }
-
-    movesLeft.setText("MOVES LEFT: " + showMoves);
-    gameTime.setText("TIME: " + game_time + "s");
-    finalScore.setText("FINAL SCORE: " + score);
+    if ((cardsLeft3.equals("NO"))) {
+      gameStatus3.setText("YOU WON!!");
+    } else if (cardsLeft3.equals("YES") && showMoves3.equals("0")) {
+      gameStatus3.setText("YOU LOST :(");
+    }
+    if ((cardsLeft1.equals("NO"))) {
+      gameStatus1.setText("YOU WON!!");
+    } else {
+      gameStatus1.setText("YOU LOST :(");
+    }
+    gameTime1.setText("TIME: " + game_time1 + "s");
+    finalScore1.setText("FINAL SCORE: " + score1);
+    movesLeft2.setText("MOVES LEFT: " + showMoves2);
+    gameTime2.setText("TIME: " + game_time2 + "s");
+    finalScore2.setText("FINAL SCORE: " + score2);
+    movesLeft3.setText("MOVES LEFT: " + showMoves3);
+    gameTime3.setText("TIME LEFT: " + game_time3 + "s");
+    finalScore3.setText("FINAL SCORE: " + score3);
   }
 }
