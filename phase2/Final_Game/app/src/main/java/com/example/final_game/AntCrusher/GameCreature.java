4cada2c7a0ded91d22eedebf8b2a1cffd88c190e
Kartik-Sangwan <kartiksangwan29@gmail.com>
2019-11-29 01:29:20 -0500

    Made the class abstract and added getter's and setter's for the instance varibales. Now this class acts a super Model class for all the subclasses of gameCreature in the game.

--- phase2/Final_Game/app/src/main/java/com/example/final_game/AntCrusher/GameCreature.java
+++ phase2/Final_Game/app/src/main/java/com/example/final_game/AntCrusher/GameCreature.java
@@ -4,13 +4,14 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
 /** Super class for all creatures and items created.*/
-public class GameCreature {
+public abstract class GameCreature {
 
   Bitmap image;
   private final int width;
   private final int height;
   private int x;
   private int y;
+  private int speed;
 
   GameCreature(Bitmap image, int x, int y) {
     this.image = image;
@@ -39,15 +40,21 @@ public class GameCreature {
   int getX() {
     return x;
   }
+
+  public void setSpeed(int speed) {
+    this.speed = speed;
+  }
+
+  public int getSpeed() {
+    return speed;
+  }
+
   /** Getter for Y coordinate of creature.*/
   int getY() {
     return y;
   }
 
-  public void draw(Canvas canvas) {
-
-  }
-
-  public void update() {
-  }
+  abstract public void update();
+  abstract public void draw(Canvas canvas);
+  abstract public void setSpeedPos(int speed, int pos);
 }
