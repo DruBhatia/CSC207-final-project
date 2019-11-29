b477900d87d5c4b5d733d4a847778ff82542e895
Kartik-Sangwan <kartiksangwan29@gmail.com>
2019-11-29 01:31:27 -0500

    implemented the abstract methods for Donut.

--- phase2/Final_Game/app/src/main/java/com/example/final_game/AntCrusher/Donut.java
+++ phase2/Final_Game/app/src/main/java/com/example/final_game/AntCrusher/Donut.java
@@ -5,11 +5,21 @@ import android.graphics.Canvas;
 /** Creates the Donut. */
 class Donut extends GameCreature {
 
-  Donut(Bitmap image, int x, int y, DonutView donutView) {
+  Donut(Bitmap image, int x, int y) {
     super(image, x, y);
   }
   /** Draws the donut on the canvas.*/
   public void draw(Canvas canvas) {
     canvas.drawBitmap(image, this.getX(), this.getY(), null);
+  }
+
+  @Override
+  public void setSpeedPos(int speed, int pos) {
+
+  }
+
+  @Override
+  public void update() {
+
   }
 }
