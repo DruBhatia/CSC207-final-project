d5a234641929e55136a0f44580d40d522777b2d7
Kartik-Sangwan <kartiksangwan29@gmail.com>
2019-11-29 10:53:16 -0500

    Created a RandomAnt class that is an Ant but just moves in a zig-zag random pattern.

---
+++ phase2/Final_Game/app/src/main/java/com/example/final_game/AntCrusher/RandomAnt.java
@@ -0,0 +1,27 @@
+package com.example.final_game.AntCrusher;
+
+import android.graphics.Bitmap;
+
+public class RandomAnt extends Ant implements AntMove{
+
+    /**
+     * Creates an RandomAnt a ant that moves in a zig-zag pattern.
+     *
+     * @param image
+     * @param x
+     * @param y
+     * @param speed
+     */
+    RandomAnt(Bitmap image, int x, int y, int speed) {
+        super(image, x, y, speed);
+    }
+
+    public void update(){
+        int randomSpeed = (int) (Math.random() * getSpeed() * 2);
+        this.setY(this.getY() - randomSpeed);
+        int i[] = {-1,1};
+        this.setX(this.getX() + i[(int) Math.ceil(Math.random()-0.5)] * randomSpeed);
+        if (this.getX() < 150) this.setX(this.getX() + 50);
+        else if (this.getX() > 700) this.setX(this.getX() - 50);
+    }
+}
