242c8345afe9f0aacac81fdeeadb2ec63b874edf
Ayush Kumar <ayushk2020@gmail.com>
2019-12-01 16:27:47 -0500

    Merge branch 'master' of /home/swoosh/StudioProjects/group_0599 with conflicts.

--- phase2/Final_Game/app/src/main/java/com/example/final_game/TrueBlueAdventure/PowerUp.java
+++ phase2/Final_Game/app/src/main/java/com/example/final_game/TrueBlueAdventure/PowerUp.java
@@ -1,14 +1,20 @@
 package com.example.final_game.TrueBlueAdventure;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import java.util.Random;
+
 import com.example.final_game.R;
 
-class PowerUp extends Pickup {
-  PowerUp(TrueBlueView gv) {
-    super(gv, R.drawable.powerup, 1400, -400);
-  }
+class PowerUp extends Pickup{
+    public PowerUp(TrueBlueView gv){
+        super(gv,R.drawable.powerup,1000,-400);
+    }
 
-  @Override
-  public void pickupAction() {
-    getGv().increaseScore();
-  }
+    @Override
+    public void pickupAction() {
+        getGv().increaseScore();
+    }
 }
